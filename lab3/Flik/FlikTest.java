2690a186ba043e9c08004cffaf012fd0522e6b57
David Hsieh <yen888yen888@gmail.com>
2021-04-03 11:39:22 +0800

    Completes lab 3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+
+    @Test
+    public void testFlik() {
+        int a = 128;
+        int b = 128;
+        int c = 129;
+        boolean same = Flik.isSameNumber(a, b);
+        boolean diff = Flik.isSameNumber(a, c);
+        assertTrue(same);
+        assertFalse(diff);
+    }
+}
