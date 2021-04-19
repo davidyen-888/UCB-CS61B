f2e92b080e0fa2ca1fd0915211c9c91a46bcadd1
David Hsieh <yen888yen888@gmail.com>
2021-04-19 22:12:00 +0800

    Creates Arrayset exercise.

--- exercises/DIY/inheritance4/ArraySet.java
+++ exercises/DIY/inheritance4/ArraySet.java
@@ -1,23 +1,41 @@
 public class ArraySet<T> {
+    private T[] items;
+    private int size;
 
+    /** Constructor for Arrayset */
     public ArraySet() {
+        items = new Object[100];
+        size = 0;
     }
 
-    /* Returns true if this map contains a mapping for the specified key.
+    /*
+     * Returns true if this map contains a mapping for the specified key.
      */
     public boolean contains(T x) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(x)) {
+                return true;
+            }
+        }
         return false;
     }
 
-    /* Associates the specified value with the specified key in this map. 
-       Throws an IllegalArgumentException if the key is null. */
+    /*
+     * Associates the specified value with the specified key in this map. Throws an
+     * IllegalArgumentException if the key is null.
+     */
     public void add(T x) {
-        return;
+        // With only one copy in set.
+        if (contains(x)) {
+            return;
+        }
+        items[size] = x;
+        size++;
     }
 
     /* Returns the number of key-value mappings in this map. */
     public int size() {
-        return 0;
+        return size;
     }
 
     public static void main(String[] args) {
@@ -26,14 +44,13 @@ public class ArraySet<T> {
         s.add("horse");
         s.add("fish");
         s.add("house");
-        s.add("fish");        
-        System.out.println(s.contains("horse"));        
-        System.out.println(s.size());       
+        s.add("fish");
+        System.out.println(s.contains("horse"));
+        System.out.println(s.size());
     }
 
-    /* Also to do:
-    1. Make ArraySet implement the Iterable<T> interface.
-    2. Implement a toString method.
-    3. Implement an equals() method.
-    */
+    /*
+     * Also to do: 1. Make ArraySet implement the Iterable<T> interface. 2.
+     * Implement a toString method. 3. Implement an equals() method.
+     */
 }
