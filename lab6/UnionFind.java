4244758cd72e7d5640582b335d84e64793fb1519
David Hsieh <yen888yen888@gmail.com>
2021-05-12 20:52:17 +0800

    Complete UnionFind

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -16,7 +16,7 @@ public class UnionFind {
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int v1) {
         if (v1 < 0 || v1 > set.length - 1) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Invalid index");
         }
     }
 
@@ -47,20 +47,39 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO, require correction.
-        int i = find(v1);
-        int j = find(v2);
-        set[i] = j;
+        // TODO
+        validate(v1);
+        validate(v2);
+        int v1Root = find(v1);
+        int v2Root = find(v2);
+        if (connected(v1Root, v2Root)) {
+            return;
+        }
+        // Always link root of smaller tree to larger tree.
+        if (sizeOf(v1Root) > sizeOf(v2Root)) {
+            set[v1Root] -= sizeOf(v2Root);
+            set[v2Root] = v1Root;
+        } else {
+            set[v2Root] -= sizeOf(v1Root);
+            set[v1Root] = v2Root;
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int v1) {
-        // TODO, not employed with Path-compression yet.
-        while (set[v1] >= 0) {
-            v1 = set[v1];
+        // TODO
+        /**while (set[v1] >= 0) {
+         v1 = set[v1];
+         }
+         return v1;*/
+        validate(v1);
+        if (set[v1] < 0) {
+            return v1;
+        } else {
+            set[v1] = find(parent(v1));
+            return set[v1];
         }
-        return v1;
     }
 
 }
--- lab6/testUnionFind.java
+++ lab6/testUnionFind.java
@@ -5,23 +5,20 @@ import static org.junit.Assert.*;
 public class testUnionFind {
     @Test
     public void testConnect() {
-        UnionFind test = new UnionFind(5);
+        UnionFind test = new UnionFind(15);
 
         assertFalse(test.connected(1, 2));
         test.union(1, 2);
         assertTrue(test.connected(1, 2));
         test.union(3, 4);
-        assertTrue(test.connected(3, 4));
         assertFalse(test.connected(2, 3));
         test.union(2, 3);
-        assertTrue(test.connected(2, 3));
-    }
-
-    @Test
-    public void testSizeOf() {
-        UnionFind test = new UnionFind(8);
-
-        assertEquals(1, test.sizeOf(1));
-        assertEquals(-1, test.parent(1));
+        assertEquals(4, test.parent(2));
+        assertEquals(4, test.sizeOf(4));
+        test.union(1, 3);
+        assertEquals(4, test.parent(1));    // Path-compression
+        test.union(2, 5);
+        test.union(0, 2);
+        assertEquals(4, test.parent(0));
     }
 }
