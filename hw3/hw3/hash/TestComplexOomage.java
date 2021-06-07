a62ea3653b20521754ab5fc45235d50b5b08909b
David Hsieh <yen888yen888@gmail.com>
2021-06-07 17:00:05 +0800

    Complete hw3

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,10 +1,12 @@
 package hw3.hash;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestComplexOomage {
 
@@ -20,7 +22,7 @@ public class TestComplexOomage {
     /* This should pass if your OomageTestUtility.haveNiceHashCodeSpread
        is correct. This is true even though our given ComplexOomage class
        has a flawed hashCode. */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -30,20 +32,26 @@ public class TestComplexOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
         // Your code here.
-
-        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+        for (int i = 0; i < 10; i++) {
+            ArrayList deadlyParam = new ArrayList();
+            for (int j = 0; j < 10; j++) {
+                deadlyParam.add(1);
+            }
+            ComplexOomage com = new ComplexOomage(deadlyParam);
+            deadlyList.add(com);
+        }
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));   // Should fail.
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
