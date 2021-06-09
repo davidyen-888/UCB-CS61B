db46eb9ac76ee9f9ed11b1c136e3a502cee27d68
David Hsieh <yen888yen888@gmail.com>
2021-06-09 15:12:18 +0800

    Add MyHashMap

---
+++ lab8/MyHashMap.java
@@ -0,0 +1,61 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    public MyHashMap() {
+
+    }
+
+    public MyHashMap(int initialSize) {
+
+    }
+
+    public MyHashMap(int initialSize, double loadFactor) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
