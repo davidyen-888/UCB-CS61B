cb0a878edc0e0f1f66ebd7abe1d08129be8c5831
David Hsieh <yen888yen888@gmail.com>
2021-06-28 16:40:27 +0800

    Complete testNearestRandom

--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -2,9 +2,11 @@ package bearmaps;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class KDTreeTest {
@@ -32,8 +34,37 @@ public class KDTreeTest {
     /* @Source: https://docs.google.com/presentation/d/1DNunK22t-4OU_9c-OBgKkMAdly9aZQkWuv_tBkDg1G4/edit#slide=id.g54b6045cf5_150_2806 */
     public void testNearest() {
         KDTree kd = buildLectureTree();
-        Point actual=kd.nearest(0,7);
-        Point expected = new Point(1,5);
+        Point actual = kd.nearest(0, 7);
+        Point expected = new Point(1, 5);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNearestRandom() {
+        Random r = new Random(500);
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            points.add(new Point(r.nextDouble(), r.nextDouble()));
+        }
+        NaivePointSet nps = new NaivePointSet(points);
+        KDTree kd = new KDTree(points);
+        long time1 = 0;
+        long time2 = 0;
+        for (int i = 0; i < 1000; i++) {
+            double rX = r.nextDouble();
+            double rY = r.nextDouble();
+            long start1 = System.currentTimeMillis();
+            Point pNPS = nps.nearest(rX, rY);
+            long end1 = System.currentTimeMillis();
+            time1 += end1 - start1;
+            long start2 = System.currentTimeMillis();
+            Point pKD = kd.nearest(rX, rY);
+            long end2 = System.currentTimeMillis();
+            time2 += end2 - start2;
+            assertEquals(pNPS.getX(), pKD.getX(), 0.01);
+            assertEquals(pNPS.getY(), pKD.getY(), 0.01);
+        }
+        System.out.println("Total time elapsed of NaivePointSet: " + time1 / 1000.0 + " seconds.");
+        System.out.println("Total time elapsed of KDTree: " + time2 / 1000.0 + " seconds.");
     }
 }
